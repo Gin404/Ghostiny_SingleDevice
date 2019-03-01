ea022669cc6f1ab6b3ad9870ccf00ba246428c4f
zang jin <930522268@qq.com>
2019-03-01 15:29:06 +0800

    add files from zpw

--- app/src/main/java/com/example/ghostiny_singledevice/GameContinueActivity.java
+++ app/src/main/java/com/example/ghostiny_singledevice/GameContinueActivity.java
@@ -3,12 +3,11 @@ package com.example.ghostiny_singledevice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class GameContinueActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        // TODO: 25/02/2019  
+        setContentView(R.layout.activity_game_continue);
     }
 }
