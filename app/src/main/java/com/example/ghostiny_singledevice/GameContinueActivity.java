ab729dec40aad3ed1354db3829e0ee5c0021c8ec
zang jin <930522268@qq.com>
2019-03-02 15:12:18 +0800

    March 2nd

--- app/src/main/java/com/example/ghostiny_singledevice/GameContinueActivity.java
+++ app/src/main/java/com/example/ghostiny_singledevice/GameContinueActivity.java
@@ -1,13 +1,29 @@
 package com.example.ghostiny_singledevice;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class GameContinueActivity extends AppCompatActivity {
-
+    Button continuebutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_continue);
+
+        continuebutton = (Button)findViewById(R.id.buttoncontinue);
+
+        continuebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent  =  new Intent(GameContinueActivity.this,SingleNumberActivity.class );
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
