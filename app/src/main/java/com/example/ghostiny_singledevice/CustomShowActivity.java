2793ef0340246b37bb5ae31e6d790d158d82090f
zang jin <930522268@qq.com>
2019-03-08 12:38:34 +0800

    显示自定义相机的照片

---
+++ app/src/main/java/com/example/ghostiny_singledevice/CustomShowActivity.java
@@ -0,0 +1,94 @@
+package com.example.ghostiny_singledevice;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.example.ghostiny_singledevice.utils.Colour;
+import com.example.ghostiny_singledevice.utils.ImageTools;
+
+import java.io.FileNotFoundException;
+
+public class CustomShowActivity extends AppCompatActivity {
+    private ImageView photo;
+    private boolean hit;
+    private Bitmap res;
+    private Button cont;
+    private boolean con = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_show);
+
+        photo = (ImageView)findViewById(R.id.custom_photo);
+        Intent intent = getIntent();
+        String imageUri = intent.getStringExtra("photo");
+        Colour choice = (Colour)intent.getSerializableExtra("color");
+        Colour unluck = (Colour)intent.getSerializableExtra("unluck");
+
+
+
+        try {
+            Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(Uri.parse(imageUri)));
+            Bitmap icon = BitmapFactory.decodeResource(getResources(),R.drawable.screamicon);
+
+            bitmap = ImageTools.rotate(bitmap, 90);
+
+            if (choice.equals(unluck)){
+                res = ImageTools.merge(bitmap, icon);
+                con = false;
+                cont.setText("Menu");
+            }else {
+                hit = ImageTools.colorRecg(bitmap, choice, 0.5);
+                if (hit){
+                    res = bitmap;
+                    con = true;
+                    //cont.setText("Continue");
+                }else {
+                    res = ImageTools.merge(bitmap, icon);
+                    con = false;
+                    cont.setText("Menu");
+                }
+            }
+
+            photo.setImageBitmap(bitmap);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        photo.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN){
+                    if (cont.getVisibility() == View.VISIBLE){
+                        cont.setVisibility(View.GONE);
+                    }else {
+                        cont.setVisibility(View.VISIBLE);
+                    }
+                }
+                return false;
+            }
+        });
+
+        cont.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1;
+                if (con){
+                    intent1 = new Intent(CustomShowActivity.this, GameActivity.class);
+                }else {
+                    intent1 = new Intent(CustomShowActivity.this, MainActivity.class);
+                }
+            }
+        });
+    }
+}
