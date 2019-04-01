3527d7496737d4244c88fcd0d385c827f1ec642b
ZPW <976664370@qq.com>
2019-04-01 21:38:06 +0800

    zpw:解决setting中两个switch保存状态的问题

--- app/src/main/java/com/example/ghostiny_singledevice/SettingActivity.java
+++ app/src/main/java/com/example/ghostiny_singledevice/SettingActivity.java
@@ -3,6 +3,7 @@ package com.example.ghostiny_singledevice;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.net.Uri;
 import android.net.wifi.WifiManager;
@@ -23,6 +24,8 @@ public class SettingActivity extends AppCompatActivity {
     Vibrator vibrator;
     ImageView mail;
     ImageButton back;
+    SharedPreferences spre,vpre;
+    final boolean falg = true,f=true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,28 +63,61 @@ public class SettingActivity extends AppCompatActivity {
             }
         }*/
 
+        int maxStreamRing = audioManager.getStreamMaxVolume(AudioManager.STREAM_RING); //最大音量
+        final int curStreamRing = audioManager.getStreamVolume(AudioManager.STREAM_RING);//当前音量
+
+        spre = getSharedPreferences("user", Context.MODE_PRIVATE);
+        if (spre != null) {
+            boolean name = spre.getBoolean("flag", falg);
+            switch1.setChecked(name);
+        }
+
         switch1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(switch1.isChecked()) {
                     //textview.setText("Switch 1 check ON by click on it");
                     audioManager.setSpeakerphoneOn(true);
+                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,curStreamRing,AudioManager.FLAG_SHOW_UI);
+                    SharedPreferences preferences = getSharedPreferences("user", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putBoolean("flag", true);
+                    editor.commit();
                 }
                 else {
                     //textview.setText("Switch 1 check OFF by click on it");
-                    audioManager.setSpeakerphoneOn(false);
+                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,0,AudioManager.FLAG_SHOW_UI);
+                    SharedPreferences preferences = getSharedPreferences("user", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putBoolean("flag", false);
+                    editor.commit();
                 }
             }
         });
+
+
+        vpre = getSharedPreferences("user", Context.MODE_PRIVATE);
+        if (vpre != null) {
+            boolean name = vpre.getBoolean("vf", f);
+            switch2.setChecked(name);
+        }
 
         switch2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(switch2.isChecked()) {
                     vibrator.vibrate(1000);
+                    SharedPreferences preferences = getSharedPreferences("user", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putBoolean("vf", true);
+                    editor.commit();
                 }
                 else {
                     vibrator.cancel();
+                    SharedPreferences preferences = getSharedPreferences("user", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putBoolean("vf", false);
+                    editor.commit();
                 }
             }
         });
