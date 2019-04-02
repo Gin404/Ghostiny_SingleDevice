7d81cd59377ee0be53b34e8f25b08d6ba416b0a8
Pancake <hyching7@gmail.com>
2019-04-02 16:21:13 +0800

    single-UI更新

--- app/src/main/java/com/example/ghostiny_singledevice/GameContinueActivity.java
+++ app/src/main/java/com/example/ghostiny_singledevice/GameContinueActivity.java
@@ -1,6 +1,8 @@
 package com.example.ghostiny_singledevice;
 
 import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,6 +17,10 @@ public class GameContinueActivity extends AppCompatActivity {
 
         continuebutton = (Button)findViewById(R.id.button_continue);
 
+        AssetManager mgr=getAssets();        //設置字體
+        Typeface typeface=Typeface.createFromAsset(mgr,"font/TM.ttf");
+        continuebutton.setTypeface(typeface);
+
         continuebutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -22,7 +28,6 @@ public class GameContinueActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-
     }
+
 }
